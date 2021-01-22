2c86f21423b27e4a62b88b355ed402d42db9bd6b
ömer nalçacı <nalcaciomer19@hotmail.com>
2021-01-22 15:04:58 +0300

    Add project files.

---
+++ DictionaryDemo/MyDictionary.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DictionaryDemo
+{
+    class MyDictionary<TKey,TValue>
+    {
+        TKey[] keys;
+        TValue[] values;
+        bool canAdd = true;
+        public MyDictionary()
+        {
+            keys = new TKey[0];
+            values = new TValue[0];
+        }
+
+        public void Add(TKey key, TValue value)
+        {
+            foreach (var item in keys)
+            {
+                if (item.Equals(key))
+                {
+                    Console.WriteLine("Eklemek istediğiniz anahtar zaten sözlükte mevcut.Lütfen başka bir anahtar deneyiniz.");
+                    canAdd = false;
+                    break;
+                }
+            }
+            if (canAdd)
+            {
+                TKey[] tempKeys = keys;
+                TValue[] tempValues = values;
+                keys = new TKey[tempKeys.Length + 1];
+                values = new TValue[tempValues.Length + 1];
+                for (int i = 0; i < tempKeys.Length; i++)
+                {
+                    keys[i] = tempKeys[i];
+                    values[i] = tempValues[i];
+                }
+                keys[keys.Length - 1] = key;
+                values[values.Length - 1] = value;
+            }
+        }
+        public int Length()
+        {
+            return keys.Length;
+        }
+    }
+}
---
+++ DictionaryDemo/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DictionaryDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Aynen derste yaptığımız MyList gibi siz de kendi Dictonary sınıfınızı yazınız. (MyDictionary)
+            //Sadece ekleme(Add) metodunu yazınız.
+
+            MyDictionary<int,string> sehirler = new MyDictionary<int,string>();
+
+            sehirler.Add(19, "Çorum");
+            sehirler.Add(19, "Ankara");
+            Console.WriteLine(sehirler.Length());
+        }
+    }
+}
